c5481f562b296ee43b6e292e88b9829f0ff18686
crystinaintan <7316016@student.unpar.ac.id>
2019-04-09 20:25:16 +0700

    Membuat query untuk membuat table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -13,14 +14,20 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamps();
-            $table->string('password');
-        });
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->string('name');
+        //     $table->string('username');
+        //     $table->string('email')->unique();
+        //     $table->timestamps();
+        //     $table->string('password');
+        // });
+        //ganti jadi gini
+        DB::statement(
+            'CREATE TABLE users (name varchar(50), id int not null AUTO_INCREMENT, statusAktif int, tglLahir date,
+             tglGabung date, alamat varchar(50), username varchar(20), email varchar(50), password varchar(255), created_at date,
+             updated_at date, primary key (id))'
+        );
     }
 
     /**
@@ -30,6 +37,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        // Schema::dropIfExists('users');
+        DB::statement(
+            'DROP TABLE users'
+        );
     }
 }
