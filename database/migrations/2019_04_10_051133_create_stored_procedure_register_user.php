9d7e52dc4f29680636ff3ef4704821db745d359c
Ferdian Benyamin <seori@ftis.unpar>
2019-04-10 13:55:55 +0700

    ProcedureRegisterUserDone

--- database/migrations/2019_04_10_051133_create_stored_procedure_register_user.php
+++ database/migrations/2019_04_10_051133_create_stored_procedure_register_user.php
@@ -14,23 +14,6 @@ class CreateStoredProcedureRegisterUser extends Migration
    */
   public function up()
   {
-    // DB::unprepared(
-    //   "CREATE PROCEDURE `RegisterUser` 
-    //        (IN name_param varchar(50),
-    //         IN status_aktif_param int,
-    //         IN tgl_lahir_param date,
-    //         IN tgl_gabung_param date,
-    //         IN alamat_param varchar(50),
-    //         IN email_param varchar(50),
-    //         IN password_param varchar(255),
-    //         IN create_at_param date,
-    //         IN updated_at_param date
-    //        )
-    //        BEGIN
-    //         INSERT INTO users(name,statusAktif,tglLahir,tglGabung,alamat,username,email,password,created_at,updated_at)
-    //         VALUES (name_param,status_aktif_param,tgl_lahir_param,tgl_gabung_param,alamat_param,email_param,password_param,create_at_param,updated_at_param)
-    //        END"
-    // );
     $sql = "CREATE PROCEDURE RegisterUser 
        (IN name_param varchar(50),
         IN status_aktif_param int,
@@ -44,7 +27,7 @@ class CreateStoredProcedureRegisterUser extends Migration
        )
        BEGIN
         INSERT INTO users(name,statusAktif,tglLahir,tglGabung,alamat,username,email,password,created_at,updated_at)
-        VALUES (name_param,status_aktif_param,tgl_lahir_param,tgl_gabung_param,alamat_param,email_param,password_param,create_at_param,updated_at_param)
+        VALUES (name_param,status_aktif_param,tgl_lahir_param,tgl_gabung_param,alamat_param,email_param,password_param,create_at_param,updated_at_param);
        END";
     DB::connection()->getPdo()->exec($sql);
   }
