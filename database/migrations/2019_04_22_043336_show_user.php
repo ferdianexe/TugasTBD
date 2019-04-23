37c25505c4daeb356a5cc707cb9637c3bfed5ba0
Stevin <michael.destiny.slayer@gmail.com>
2019-04-23 23:52:32 +0700

    Coba Coba Cursor

--- database/migrations/2019_04_22_043336_show_user.php
+++ database/migrations/2019_04_22_043336_show_user.php
@@ -26,12 +26,53 @@ class ShowUser extends Migration
 
         $sql = "CREATE PROCEDURE ShowUser ()
         BEGIN
+            DECLARE test INT;
+            DECLARE res INT;
+            SET test = 1;
+            SET res = 0;
 
-        SELECT
-            id,name,statusAktif,tglLahir,tglGabung,alamat,email,terakhirMeminjam,terakhirMemesan,hasReturned
-        FROM
-            users;
+            CREATE TABLE userDanPinjamanTerakhir(
+                id int,
+                terakhirMemesan date
+            );
+            INSERT INTO userDanPinjamanTerakhir 
+                SELECT DISTINCT id, tanggalMeminjam
+                FROM users
+                Group By id
+                Order By tanggalMeminjam desc
+            ;
 
+                -- Cursor
+                DECLARE namaCursor CURSOR FOR 
+                SELECT id From users;
+
+                -- declare NOT FOUND handler
+                DECLARE CONTINUE HANDLER 
+                        FOR NOT FOUND SET v_finished = 1;
+
+                OPEN namaCursor;
+
+                -- FETCHING 
+                get_all_id: LOOP
+                    FETCH namaCursor INTO test;
+                    
+                    IF v_finished = 1 THEN 
+                        LEAVE get_all_id;
+                    END IF;
+
+                    -- SET/masukin nilai yang di fetch tadi ke table temp (utk tambah 1 record baru)
+                    SET res = test; 
+                END LOOP get_all_id;
+                -- END FETCHING
+
+                CLOSE namaCursor;
+                -- End Cursor
+
+            SELECT
+                test,id,name,statusAktif,tglLahir,tglGabung,alamat,email,terakhirMeminjam,terakhirMemesan,hasReturned
+            FROM
+                users;
+            DROP table userDanPinjamanTerakhir;
        END";
         DB::connection()->getPdo()->exec($sql);
     }
