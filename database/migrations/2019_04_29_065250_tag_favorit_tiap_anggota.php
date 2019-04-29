c0dd3fd2f2265910a66542e1eff37136e0867b57
ferdianexe <ferdianboy@hotmail.com>
2019-04-29 21:51:51 +0700

    Laporan tag favorit per anggota done

--- database/migrations/2019_04_29_065250_tag_favorit_tiap_anggota.php
+++ database/migrations/2019_04_29_065250_tag_favorit_tiap_anggota.php
@@ -19,20 +19,40 @@ class TagFavoritTiapAnggota extends Migration
     ) 
     BEGIN
       DECLARE totalPeminjaman INT ;
+      CREATE TABLE kategoriFavorit(kodeKategoriFavorit int);
       SELECT 
         count(kodeEksemplar) INTO totalPeminjaman
       FROM 
         KumpulanPeminjaman
       WHERE 
         idUser = id_val ;
+
+
+      INSERT INTO kategoriFavorit
       SELECT 
-        kodeEksemplar 
+        kumpulanBukudankumpulanKategori.idKategori
       FROM 
         KumpulanPeminjaman
+      INNER JOIN kumpulanEksemplar on kumpulanEksemplar.kodeEksemplar = KumpulanPeminjaman.kodeEksemplar
+      INNER JOIN kumpulanBukudankumpulanKategori on kumpulanBukudankumpulanKategori.idBuku = kumpulanEksemplar.idBuku
       WHERE 
-        idUser = id_val ;
+        KumpulanPeminjaman.idUser = id_val ;
+      SELECT * 
+      FROM KumpulanKategori
+      INNER JOIN (
+        SELECT kodeKategoriFavorit,count(kodeKategoriFavorit) as total
+        FROM kategoriFavorit
+        GROUP BY kodeKategoriFavorit
+      ) as table2 on table2.kodeKategoriFavorit = KumpulanKategori.idKategori
+      ORDER BY table2.total DESC ;
+      
+      DROP TABLE kategoriFavorit;
     END";
 
+
+    DB::connection()->getPdo()->exec($sql);
+
+    
     $sql = "CREATE PROCEDURE tambahEksemplar
     (
       IN kode_eksemplar_val INT,
@@ -64,9 +84,9 @@ class TagFavoritTiapAnggota extends Migration
    */
   public function down()
   {
-    // DB::unprepared(
-    //   "DROP PROCEDURE TagFavoritPerAnggota"
-    // );
+    DB::unprepared(
+      "DROP PROCEDURE TagFavoritPerAnggota"
+    );
     DB::unprepared(
       "DROP PROCEDURE tambahEksemplar"
     );
