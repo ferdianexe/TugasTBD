a6147f484338b584fdb9dedb6fbbf65a1b6edaaf
crystinaintan <7316016@student.unpar.ac.id>
2019-04-29 15:51:46 +0700

    SPUntukLaporanBukuTerfavorite

---
+++ database/migrations/2019_04_29_070635_create_stored_procedure_buku_terfavorite.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoredProcedureBukuTerfavorite extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $sql = "CREATE PROCEDURE ShowBukuTerfavorite()
+        BEGIN
+            SELECT 
+                kumpulanbuku.nama, count(temp.idPeminjaman) as jumlahPeminjaman
+            FROM
+                kumpulanbuku
+                INNER JOIN 
+                ( SELECT kumpulanpeminjaman.idPeminjaman, kumpulaneksemplar.idBuku
+                  FROM
+                      kumpulanpeminjaman 
+                      INNER JOIN kumpulaneksemplar ON kumpulanpeminjaman.kodeEksemplar = kumpulaneksemplar.kodeEksemplar) AS temp
+                ON kumpulanbuku.idBuku = temp.idBuku
+            GROUP BY
+                kumpulanbuku.nama;
+        END";
+
+        DB::connection()->getPdo()->exec($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared(
+            "DROP PROCEDURE ShowBukuTerfavorite"
+        );
+    }
+}
