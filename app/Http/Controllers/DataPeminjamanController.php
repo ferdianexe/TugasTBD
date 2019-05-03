edf76ac836fba94a669ddd5c79342a77ae383ef0
ferdianexe <ferdianbenyamin@gmail.com>
2019-05-03 23:21:42 +0700

    Dokumentasi

--- app/Http/Controllers/DataPeminjamanController.php
+++ app/Http/Controllers/DataPeminjamanController.php
@@ -33,24 +33,24 @@ class DataPeminjamanController extends Controller
       $sql2 = "CALL ShowPeminjamanWithOffset ('$id','$page')";
     }
     
-    $kumpulanPeminjaman = DB::select($sql);
+    $kumpulanPeminjaman = DB::select($sql); //cuman mau dapet totalbanyaknya buat dapet row
     $counter = ceil((count($kumpulanPeminjaman)*1.0)/10)-1;
-    $paginationPage = array();
-    $previousPage = array();
-    $startVal = (int)($page/10);
-    $continousPagination = ($page%10);
+    $paginationPage = array(); //pagination tombol2 setelah page sekarang yang sedia
+    $previousPage = array(); // pagination tombol2 page sebelum page yang sekarang
+    $startVal = (int)($page/10); //mendapatkan nilai kelipatan sebagai pengali awal (page per 10 untuk sekarang)
+    $continousPagination = ($page%10); //mendapatkan digit terakhirnya sebagai lanjutnya
     for($i = 1 ; $i<10;$i++){
       if(($page-9+$i)>1){
-        array_push ($previousPage,($page-9+$i));
+        array_push ($previousPage,($page-9+$i)); //masukan selama page tidak negatif
       }
-      if(($startVal+$continousPagination+$i)==1)continue;
-      if(($startVal*10)+$continousPagination+$i>=$counter) continue;
+      if(($startVal+$continousPagination+$i)==1)continue; // masukan selama page bukan 1 (1 ga masuk looping)
+      if(($startVal*10)+$continousPagination+$i>=$counter) continue; // masukan selama masih tersedia didalam counter
         array_push ($paginationPage,(($startVal*10)+$continousPagination+$i));
       }
-      if($page != $counter){
+      if($page != $counter){ //jika page = counter tidak perlu dimasukan karena ujung page selalu di print
         array_push ($paginationPage, $counter);
       }
-    $kumpulanPeminjaman = DB::select($sql2);
+    $kumpulanPeminjaman = DB::select($sql2); //query sebenarnya yang mmenggunakan offset
     return view('TampilanDataPeminjaman',compact('isAdmin','kumpulanPeminjaman','paginationPage','page','previousPage'));
   }
 }
